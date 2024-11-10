5f1d47cb63bee6121b8f6187a91b3ee511a292ec
cel004 <ngcelinaa@gmail.com>
2024-11-10 02:18:45 -0500

    Fix: "common_parent is null" error

--- cones/ConeDrag.cs
+++ cones/ConeDrag.cs
@@ -1,15 +1,44 @@
 using Godot;
-using System;
 
-public partial class ConeDrag : Node
+public partial class ConeDrag : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    private bool _selected = false;
+    private Vector2 mouseOffset = Vector2.Zero;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Process(double delta)     // if mouse left clicks = object follows mouse
+    {
+        if (_selected)
+        {
+            FollowMouse();
+        }
+    }
+
+    
+    private void FollowMouse()
+    {
+        GlobalPosition = GetViewport().GetMousePosition() + mouseOffset;    // pos = pos of mouse + mouse offset -> smooth dragging
+    }
+
+
+    private void _OnArea2DInputEvent(Node viewport, InputEvent @event, long shapeIdx)//+
+    {
+        if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Left)
+        {
+            if (mouseEvent.Pressed)
+            {
+                mouseOffset = GlobalPosition - GetViewport().GetMousePosition();
+                _selected = true;
+            }
+            else{
+                // not selected
+                _selected = false;
+            }
+        }
+    }
+
+    public override void _Ready()
+    {
+        var area = GetNode<Area2D>("pileOfCones");
+        area.InputEvent += _OnArea2DInputEvent;
+    }
 }
